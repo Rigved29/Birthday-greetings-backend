4c97cb082aba82ed943aa50406beaf5fbff6330c
Rigved <rigved.shrivastava232@gmail.com>
2022-10-19 23:47:24 +0530

    adding mongodb connection code

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("DB Connection successful");
+  });
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello" });
