ce2db7dba75f245d7fa29359fe422485ef83b30f
Rigved <rigved.shrivastava232@gmail.com>
2022-12-04 21:15:42 +0530

    added code for cors problem fixes

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var allowCrossDomain = function (req, res, next) {
   );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 };
 
