2aefa0d83ff8b12a159ea6c37b5e998d46912d86
Rigved <rigved.shrivastava232@gmail.com>
2022-12-06 22:45:27 +0530

    added code cors problem fixes

--- app.js
+++ app.js
@@ -8,11 +8,8 @@ app.use(express.json());
 
 // For resolving cors problem
 var allowCrossDomain = function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Origin",
-    `https://birthday-wish-alpha.vercel.app`
-  );
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); https://birthday-wish-alpha.vercel.app
+  res.header("Access-Control-Allow-Origin", `*`);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", true);
